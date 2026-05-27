5416ed69f6ba93a1f2d26c36c96541a09f7ea5be
Viktor <viktor.bacik@academicschool.cz>
2026-05-27 11:38:32 +0200

    added json beggining, in progress - repair of using it as is

--- Film_app/Program.cs
+++ Film_app/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System.Text.Json;
+class Program
+    
+class SearchResponse
+{
+    public List<Movie> Search { get; set; }
+}
+
+class Movie
+{
+    public string Title { get; set; }
+    public string Year { get; set; }
+}
+{
+    static async Task Main()
+    {
+        var client = new HttpClient();
+        Console.Write("Zadej film: ");
+        string input = Console.ReadLine();
+        string url = $"https://www.omdbapi.com/?apikey=f5b1565b&s={input}";
+        var response = await client.GetAsync(url);
+        string data = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(data);
+        
+    }
+}
